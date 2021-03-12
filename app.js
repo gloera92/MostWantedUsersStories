4ca826c43e097c72cd380cf999c7b595fdb9402c
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-03-12 16:16:20 -0500

    added gender function and updated name function, so that user can populate whole family tree.

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ function searchByName(){
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
         }
-        return false;
+        else {return false;
+        }
     });
 
     // Rather than console logging, you need to append the filteredPeople to a table.
@@ -24,6 +25,20 @@ function searchByName(){
 
 let searchResult = searchByName({people});
 console.log(searchResult);
+
+function searchByGender(){
+    let genderInput = document.forms[ 'nameForm']['gender'].value;
+    let filteredGender = people.filter(function(person){
+        if(person.gender === genderInput){
+            return true;
+        }
+        return false;
+    });
+
+
+
+}
+
 
 // function searchByGender(gender) {
 //     let genderResult =
