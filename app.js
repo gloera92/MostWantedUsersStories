a8835b5cc88818f0e34e066c935ce26f1eef4c5b
robert mix <eurotuner1981@gmail.com>
2021-03-13 14:40:48 +0100

    added functions through currentspouse

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function searchByGender(){
         if(filteredGender.length > 0){
             console.log(filteredGender);
         }else{
-            console.log('confused gender');
+            console.log("Gender is male or female!");
         }
 }
 
@@ -64,7 +64,7 @@ function searchByDob(){
         if(filteredDob.length > 0){
             console.log(filteredDob);
         }else{
-            console.log('Do they even know thier DOB?');
+            console.log('Do you know thier DOB?');
         }
 }
 
@@ -72,18 +72,140 @@ function searchByDob(){
 let dobResult = searchByDob({people});
 console.log(dobResult);
 
+// height search
+
+function searchByHeight(){
+    let heightInput = document.forms[ 'nameForm']['height'].value;
+    let filteredHeight = people.filter(function(person){
+        if(person.height == heightInput){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+        if(filteredHeight.length > 0){
+            console.log(filteredHeight);
+        }else{
+            console.log('Do you know their Hieght?');
+        }
+}
 
 
+let heightResult = searchByHeight({people});
+console.log(heightResult);
+
+// height search
+
+function searchByWeight(){
+    let weightInput = document.forms[ 'nameForm']['weight'].value;
+    let filteredWeight = people.filter(function(person){
+        if(person.weight == weightInput){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+        if(filteredWeight.length > 0){
+            console.log(filteredWeight);
+        }else{
+            console.log('Do you know their Hieght?');
+        }
+}
 
 
+let weightResult = searchByWeight({people});
+console.log(weightResult);
+
+// eyecolor search
+
+function searchByEyecolor(){
+    let eyecolorInput = document.forms[ 'nameForm']['eyecolor'].value;
+    let filteredEyecolor = people.filter(function(person){
+        if(person.eyecolor == eyecolorInput){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+        if(filteredEyecolor.length > 0){
+            console.log(filteredEyecolor);
+        }else{
+            console.log('Pick a different color!');
+        }
+}
 
 
+let eyecolorResult = searchByEyecolor({people});
+console.log(eyecolorResult);
 
-console.log(document.forms["nameForm"]['gender'].value);
-console.log(document.forms["nameForm"]['dob'].value);
-console.log(document.forms["nameForm"]['height'].value);
-console.log(document.forms["nameForm"]['weight'].value);
-console.log(document.forms["nameForm"]['eyecolor'].value);
-console.log(document.forms["nameForm"]['occupation'].value);
-console.log(document.forms["nameForm"]['parents'].value);
-console.log(document.forms["nameForm"]['currentspouse'].value);
+// occupation search
+
+function searchByOccupation(){
+    let occupationInput = document.forms[ 'nameForm']['occupation'].value;
+    let filteredOccupation = people.filter(function(person){
+        if(person.occupation == occupationInput){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+        if(filteredOccupation.length > 0){
+            console.log(filteredOccupation);
+        }else{
+            console.log('Do you know their occupation?');
+        }
+}
+
+
+let occupationResult = searchByOccupation({people});
+console.log(occupationResult);
+
+// Parents search
+
+function searchByParents(){
+    let parentsInput = document.forms[ 'nameForm']['parents'].value;
+    let filteredParents = people.filter(function(person){
+        if(person.parents == parentsInput){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+        if(filteredParents.length > 0){
+            console.log(filteredParents);
+        }else{
+            console.log('Do you know their parents?');
+        }
+}
+
+
+let parentsResult = searchByParents({people});
+console.log(parentsResult);
+
+// currentspouse search
+
+function searchByCurrentspouse(){
+    let currentSpouseInput = document.forms[ 'nameForm']['currentspouse'].value;
+    let filteredCurrentSpouse = people.filter(function(person){
+        if(person.currentSpouse == currentSpouseInput){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+        if(filteredCurrentSpouse.length > 0){
+            console.log(filteredCurrentSpouse);
+        }else{
+            console.log('Do you know their curentspouse?');
+        }
+}
+
+
+let currentSpouseResult = searchByCurrentspouse({people});
+console.log(currentSpouseResult);
